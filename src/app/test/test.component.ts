bbf906115d608a382712e3c92196405bf1e3128a
channu <channucs0009@gmail.com>
2019-06-17 13:44:03 +0530

    class and style binding

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+  public hasError = true;
+  public suc = 'text-success';
+  public msg = {
+    'text-success' : !this.hasError,
+    'text-danger' : this.hasError,
+    'text-special': true
+  }
+  ngOnInit() {
+  }
+
+}
