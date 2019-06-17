287b171122fafbf85ce5557fdd6ed0ca5e57eb6f
channu <channucs0009@gmail.com>
2019-06-17 15:36:34 +0530

    two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
@@ -10,6 +11,7 @@ import { TestComponent } from './test/test.component';
     TestComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -16,12 +16,16 @@ export class TestComponent implements OnInit {
     'text-special': true
   }
   public greeting = '';
+  public name = '';
   ngOnInit() {
   }
 onClick(event) {
   console.log(event);
   this.greeting = 'hello you clicked button';
+  this.name = event;
 }
+
+
 templateRefVar(value) {
   console.log(value)
 }
