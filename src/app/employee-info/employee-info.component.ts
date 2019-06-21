05fdeedecf805cc8eda5bb05d33ba496fc92f9af
channu <channucs0009@gmail.com>
2019-06-21 13:14:46 +0530

    paramMap and next and back link in card

--- src/app/employee-info/employee-info.component.ts
+++ src/app/employee-info/employee-info.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../employee.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router , ParamMap} from '@angular/router';
 @Component({
   selector: 'app-employee-info',
   templateUrl: './employee-info.component.html',
@@ -10,16 +10,37 @@ export class EmployeeInfoComponent implements OnInit {
   public emp ;
   public error ;
   public userId ;
-  constructor(private EmpService: EmployeeService , private route: ActivatedRoute ) { }
-  
+  constructor(private router: Router , private EmpService: EmployeeService , private route: ActivatedRoute ) { }
+
   ngOnInit() {
-    let id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.userId = id ;
-    
-    this.EmpService.getEmpInfo(id)
+    // let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    // this.userId = id ;
+
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt (params.get('id'));
+      this.userId = id ;
+    })
+    this.EmpService.getEmpInfo(this.userId)
     .subscribe(data => this.emp = data ,
       error => this.error = error.message );
-      
+
   }
+goBack() {
+  this.userId = this.userId - 1 ;
+  this.router.navigate(['/employeeInfo', this.userId]);
+
+  this.EmpService.getEmpInfo(this.userId)
+  .subscribe(data => this.emp = data ,
+    error => this.error = error.message );
+    
+}
+goNext() {
+  this.userId = this.userId + 1 ;
+  this.router.navigate(['/employeeInfo', this.userId]);
+
+  this.EmpService.getEmpInfo(this.userId)
+  .subscribe(data => this.emp = data ,
+    error => this.error = error.message );
+}
 
 }
